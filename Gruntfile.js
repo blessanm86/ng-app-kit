088dccac3e6f4c317cc1b03f4e635abba2486fea
Blessan Mathew <blessenm@gmail.com>
2014-05-12 20:11:46 +0530

    Now script tags are automatically injected.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,12 @@ module.exports = function(grunt) {
       
     ]
   }
+  
+  //Vendor files names in import order. Needed for script tag injection
+  var vendorScripts = vendorFiles.scripts.map(function(arr){
+    var parts = arr.split('/');
+    return parts [parts.length-1]
+  })
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -85,17 +91,17 @@ module.exports = function(grunt) {
     },
     
     uglify: {
-      dev: {
-        options: {
-          sourceMap: true,
-          sourceMapIncludeSources: true,
-          mangle: false
-        },
-        files: {
-          'temp/assets/app.js': ['temp/app/**/*.js'],
-          'temp/assets/vendor.js': vendorFiles.scripts
-        },
-      },
+      //dev: {
+      //  options: {
+      //    sourceMap: true,
+      //    sourceMapIncludeSources: true,
+      //    mangle: false
+      //  },
+      //  files: {
+      //    'temp/assets/app.js': ['temp/app/**/*.js'],
+      //    'temp/assets/vendor.js': vendorFiles.scripts
+      //  },
+      //},
       dist: {
         options: {
           mangle: true
@@ -107,10 +113,41 @@ module.exports = function(grunt) {
       }
     },
     
+    injector: {
+      options: {
+        template: 'app/index.html',
+        dest: 'temp/index.html',
+        ignorePath: ['temp'],
+        sort: function(a, b) {
+          if( a.indexOf('vendor') === -1 && b.indexOf('vendor') === -1) {
+            return -1;
+          } else if( a.indexOf('vendor') === -1) {
+            return 1;
+          } else if (b.indexOf('vendor') === -1) {
+            return -1;
+          }
+          var aSplit = a.split('/');
+          var aName = aSplit [aSplit.length-1];
+        
+          var bSplit = b.split('/');
+          var bName = bSplit[bSplit.length-1];
+        
+          return vendorScripts.indexOf(aName) - vendorScripts.indexOf(bName);
+        }
+      },
+      dev: {
+        files: [
+          {'temp/index.html': ['temp/vendor/**/*.js', 'temp/app/**/*.js']},
+          //{'temp/index.html': ['temp/app/**/*.js']}
+          //{ expand: true, cwd: 'temp/vendor/', src: ['**/*.js'], dest: 'temp/index.html' }
+        ]
+      }
+    },
+    
     ngconstant: {
       options: {
         dest: 'app/config-generated.js',
-        name: 'config',
+        name: 'config'
       },
       dev: {
         constants: grunt.file.readJSON('app/config/dev.json')
@@ -126,9 +163,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-ng-constant')
+  grunt.loadNpmTasks('grunt-ng-constant');
+  grunt.loadNpmTasks('grunt-injector')
   
-  grunt.registerTask('default', ['clean:dev', 'copy:dev', 'ngconstant:dev', 'uglify:dev', 'watch:dev']);
+  grunt.registerTask('default', ['clean:dev', 'copy:dev', 'ngconstant:dev', 'injector:dev']);
   grunt.registerTask('dist', ['clean:dist', 'copy:dist', 'ngconstant:dist', 'uglify:dist', 'connect:dist']);
 
 };
