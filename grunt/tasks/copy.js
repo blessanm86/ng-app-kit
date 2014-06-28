7b7f59dbc4d0925d49daf500dd899864c09caf8c
Blessan Mathew <blessenm@gmail.com>
2014-06-28 05:35:53 +0530

    In dist mode, public files are now copied to the dist directory.

--- grunt/tasks/copy.js
+++ grunt/tasks/copy.js
@@ -5,16 +5,16 @@ module.exports = {
     files: [
       //Copy all the public files.
       { expand: true, cwd: 'public/', src: ['**'], dest: 'temp/' },
-      
+
       //Copy all the application files.
       { expand: true, cwd: 'app/', src: ['**/*', '!index.html', '!**/styles/**'], dest: 'temp/app/' },
-      
+
       //Copy the vendor script files
       { expand: true, flatten: true, src: vendorFiles.scripts, dest: 'temp/vendor/' },
-      
+
       //Copy the css file
       { expand: true, cwd: 'app/styles/', src: ['**/*.css'], dest: 'temp/assets' },
-      
+
       //Copy app html file.
       { expand: true, cwd: 'app/', src: 'index.html', dest: 'temp/' }
     ]
@@ -22,14 +22,14 @@ module.exports = {
   dist: {
     files: [
       //Copy all the public files.
-      { expand: true, cwd: 'public/', src: ['**'], dest: 'temp/' },
-      
+      { expand: true, cwd: 'public/', src: ['**'], dest: 'dist/' },
+
       //Copy the css file
       { expand: true, cwd: 'app/styles/', src: ['**/*.css'], dest: 'dist/assets' },
-      
+
       //Copy template files.
       { expand: true, src: ['app/templates/**/*'], dest: 'dist/' },
-      
+
       //Copy app html files.
       { expand: true, cwd: 'app/', src: 'index.html', dest: 'dist/' }
     ]
